2a199b730fcf518c2cb843b8cc05232623187c9e
Cícero Alves <cicerocasj@gmail.com>
2015-05-01 16:15:59 -0300

    tarefa 1 pronta

--- js/scripts.js
+++ js/scripts.js
@@ -22,6 +22,7 @@ function vaibuscarasissues(){
             },
             'popula_tabela': function(){
                 issuestable.empty();
+                issuestable.append(line_table('Numero', 'Titulo'));
                 for(var line in issues){
                     issuestable.append(line_table(issues[line].number, issues[line].title));
                 }
@@ -30,5 +31,35 @@ function vaibuscarasissues(){
         return methods;
     }
     var le_repo = GithubRepo(username.val(), reponame.val());
+    le_repo.busca_issues();
+}
+
+function vaibuscarasissues_this(){
+    function GithubRepo(username, reponame){
+        this.username = username;
+        this.reponame = reponame;
+        this.issues;
+    }
+
+    GithubRepo.prototype.busca_issues = function(){
+        var that = this;
+        $.ajax({
+            url: "https://api.github.com/repos/"+this.username+"/"+this.reponame+"/issues"
+        }).success(
+            function(data){
+                that.issues = data;
+                that.popula_tabela();
+            }
+        );
+    };
+    GithubRepo.prototype.popula_tabela = function(){
+        issuestable.empty();
+        issuestable.append(line_table('Numero', 'Titulo'));
+        for(var line in this.issues){
+            issuestable.append(line_table(this.issues[line].number, this.issues[line].title));
+        }
+    };
+
+    var le_repo = new GithubRepo(username.val(), reponame.val());
     le_repo.busca_issues();
 }
